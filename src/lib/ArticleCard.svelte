bacfadb94fa3b35178075b7448e2c327cfb2977a
Sam Rees <sam.rees@gmail.com>
2023-02-03 13:29:27 +0000

    article card box shadow changes

--- src/lib/ArticleCard.svelte
+++ src/lib/ArticleCard.svelte
@@ -14,7 +14,7 @@
 <style>
     .card {
         text-align: center;
-        box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 4px, rgba(0, 0, 0, 0.05) 0px 0.5px 1px;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 0.5px 1px;
         transition: all .2s ease-in-out;
         margin-top: 5px;
         margin-bottom: 5px;
